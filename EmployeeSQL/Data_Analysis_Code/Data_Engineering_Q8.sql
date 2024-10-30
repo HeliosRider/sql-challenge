dffc9b89fde0c877196921245d876e483a3f93c6
Jonathan Calloway <stscsrtrd@gmail.com>
2024-10-29 22:00:37 -0400

    Update Data_Analysis_Q8.sql
     8. List the frequency counts, in descending order, of all 
          the employee last names (that is, how many employees share 
           each last name).
    
    -Code provided

--- EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q8.sql
+++ EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q8.sql
@@ -1,3 +1,6 @@
+8. List the frequency counts, in descending order, of all 
+   the employee last names (that is, how many employees share each last name).
+
 SELECT 
 	COUNT
 	(last_name) as Frequency, last_name
