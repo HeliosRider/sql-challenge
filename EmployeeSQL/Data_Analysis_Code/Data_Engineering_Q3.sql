e2a21f009461324745e89ea58c39f98729ab4d21
Jonathan Calloway <stscsrtrd@gmail.com>
2024-10-29 21:54:42 -0400

    Update Data_Analysis_Q3.sql
     3. List the manager of each department along with their department number, department .
    
    -Code provided

--- EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q3.sql
+++ EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q3.sql
@@ -1,3 +1,5 @@
+3. List the manager of each department along with their department number, department. 
+
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, 
 	   employees.last_name, employees.first_name
 FROM departments
