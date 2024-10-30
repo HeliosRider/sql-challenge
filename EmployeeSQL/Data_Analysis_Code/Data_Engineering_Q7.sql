ee1b3581956478ee113684017da95c29886a97b0
Jonathan Calloway <stscsrtrd@gmail.com>
2024-10-29 21:59:37 -0400

    Update Data_Analysis_Q7.sql
    7. List each employee in the Sales and Development departments, including their employee number,  last name, first name, and department name.
    
    -Code provided

--- EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q7.sql
+++ EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q7.sql
@@ -1,3 +1,6 @@
+7. List each employee in the Sales and Development departments, including their employee number, 
+   last name, first name, and department name.
+
 SELECT 
 	 dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM
