52ea85be16a821329118a337c709d2b3b1f90cf8
Jonathan Calloway <stscsrtrd@gmail.com>
2024-10-29 21:50:46 -0400

    Update Data_analysis_Q1.sql
       1. List the employee number, last name, first name, sex, and salary of each employee.
       -Code provided

--- EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q1.sql
+++ EmployeeSQL/Data_Analysis_Code/Data_Engineering_Q1.sql
@@ -1,3 +1,5 @@
+1. List the employee number, last name, first name, sex, and salary of each employee.
+
 SELECT 
 	employees.emp_no, employees.last_name, employees.first_name,
 	employees.sex, salaries.salary
